808805f910c85747fb44890336906a303919d082
zhufengzhufeng <894918097@qq.com>
2016-10-09 16:18:42 +0800

    arrow function

--- 3.module/a.js
+++ 3.module/a.js
@@ -1,6 +1,28 @@
 var calc = {
-    '+':(a,b)=>{a+b},
-    '-':(a,b)=>{a-b}
+    '+':(a,b)=>a+b,
+    '-':(a,b)=>a-b
 };
 //用两种方法导出
 //exports module.exports
+
+/*function sum(a,b) {
+    return a+b
+}
+let sum = (a,b) => a+b
+console.log(sum(1,2));
+是没有this指向
+*/
+
+
+/*global.b = 1;
+var obj = {
+    b:2,
+    a:function () {
+        setTimeout(()=> {
+            this.b +=2;
+            console.log(obj.b);
+        },1000);
+    }
+};
+obj.a();*/
+
