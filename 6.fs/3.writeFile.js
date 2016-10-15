a00ccc472621bc046097720bfeec580164d70fae
zhufengzhufeng <894918097@qq.com>
2016-10-15 16:10:27 +0800

    readfile writefile

---
+++ 6.fs/3.writeFile.js
@@ -0,0 +1,10 @@
+var fs  = require('fs');
+fs.writeFileSync('./1.txt',123);
+//默认写入的编码是utf8
+//默认创建
+//将提供的数据放在第二个参数上
+//fs.appendFileSync(''); 实现是调用了writeFileSync + flag:'a'
+fs.writeFile('./1.txt',123,function(err){
+    console.log(err);//写入是否有错误
+});
+//拷贝文件 写一个同步的在写一个异步的
