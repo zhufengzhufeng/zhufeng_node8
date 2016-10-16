eebb11413668e69e4c5cb83335503777a6a49a60
zhufengzhufeng <894918097@qq.com>
2016-10-16 18:15:43 +0800

    file

--- 10.http/clock/server.js
+++ 10.http/clock/server.js
@@ -3,12 +3,13 @@ var fs = require('fs');
 var mime = require('mime');
 var url = require('url');
 http.createServer(function (req,res) {
-    //当请求到来时才会执行此方法
-    //通过不同的url来返回不同的数据 路由
     var pathname = url.parse(req.url,true).pathname;
     if(pathname == '/'){
         res.setHeader('Content-Type','text/html;charset=utf8');
-        fs.createReadStream('./2.html').pipe(res);
+        fs.createReadStream('./clock.html').pipe(res);
+    }else if(pathname=='/clock'){
+        res.setHeader('Content-Type','text/plain;charset=utf8');
+        res.end(new Date().toLocaleString());
     }else{
         fs.exists('.'+pathname,function (exists) {
             if(exists){
@@ -20,3 +21,7 @@ http.createServer(function (req,res) {
         })
     }
 }).listen(3000);
+
+
+//当请求到来时才会执行此方法
+//通过不同的url来返回不同的数据 路由
